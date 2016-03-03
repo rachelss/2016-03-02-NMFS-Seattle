b8a6e79dc480bfb9774aa6755efadd294493d85d
Rachel Schwartz <rsschwartz@gmail.com>
2016-03-03 14:45:46 -0800

    add sql example

---
+++ example.sql
@@ -0,0 +1,71 @@
+SELECT 	family, 
+		personal 
+FROM 	Person;
+
+.headers on
+.mode column
+
+
+SELECT * 
+FROM Person; 
+
+.help
+	
+	
+SELECT quant FROM Survey;
+
+SELECT DISTINCT quant FROM Survey;
+
+SELECT DISTINCT 
+	taken, 
+	quant 
+FROM 
+	Survey;
+	
+	
+	
+	
+SELECT 
+	* 
+FROM 
+	person 
+ORDER BY 
+	ident;
+	
+
+SELECT 
+	taken, 
+	person, 
+	quant 
+FROM Survey 
+ORDER BY 
+	taken, 
+	person DESC
+;
+	
+<= 
+>=
+!=
+= (R only ==)
+<>	
+
+SELECT * 
+FROM Survey 
+WHERE (person='lake' 
+	OR person='roe') 
+	AND quant='sal'
+;
+
+SELECT *
+FROM Visited
+WHERE site LIKE 'D%'	
+;
+
+SELECT 
+	reading,
+	1.05*reading as adjreading   /* -- Adjusting incorrect raditaion */
+FROM 
+	Survey
+WHERE
+	quant = 'rad'
+	;
